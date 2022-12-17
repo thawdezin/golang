7b176753acb09a674d17e02006e2c5e3c331e145
thawdezin <thawdezin@gmail.com>
2022-12-18 02:21:55 +0630

    Add header

--- hello.go
+++ hello.go
@@ -7,12 +7,18 @@ import (
 )
 
 func main() {
-	// Make an HTTP GET request to the website
-	response, err := http.Get("http://thawdezin.netlify.app")
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
-	}
+	req, err := http.NewRequest("GET", "http://www.example.com", nil)
+if err != nil {
+	fmt.Printf("Error creating request: %s\n", err)
+}
+req.Header.Add("User-Agent", "My Go Client 1.0")
+
+response, err := http.DefaultClient.Do(req)
+if err != nil {
+	fmt.Printf("The HTTP request failed with error %s\n", err)
+} else {
+	data, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(data))
+}
+
 }
