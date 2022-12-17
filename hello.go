59867f37d287e025684df156d745d7564c7f1f39
thawdezin <thawdezin@gmail.com>
2022-12-18 02:02:26 +0630

    HTTP GET request

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Make an HTTP GET request to the website
+	response, err := http.Get("http://thawdezin.netlify.app")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
 }
